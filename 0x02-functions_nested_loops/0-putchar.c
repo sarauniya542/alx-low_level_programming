74554081c8aaf197af215bf5814cd30ffb3f2081
sarauniya542 <andesther97@gmail.com>
2022-07-05 11:55:11 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main-entry point
+ *
+ * RETURN:Always 0 (Success)
+ */
+
+int main(void)
+{
+cha*sh ="_putchar";
+while(*sh)
+{
+_putchar(*sh);
+sh++;
+_putchar('\n');
+return(0);
+}
+
